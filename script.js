15c3d70c096e6ddf88b2291add72a653b2ecfbd2
michaelkwan00 <michaelburps@gmail.com>
2017-10-15 02:03:33 -0700

    make everything the way i want

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-require('math')
+$(document).ready(function(){}
+	var big = Array(25,50,75,100);
+	var small = Array(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10);
+	//these are the numbers we are given
 
-function random() {
-	return math.random(1,20)
-}
+	//picking big numbers
+	$('#big').click(function()) {
+		var pick = big[Math.floor(Math.random()*big.length)];
+		$('#big').unbind('click'); //disable clicking big if all are dished out
+		$('.numbers-bar').append('<span class="big">' + pick + '</div>')
+	}
+
+
+)
